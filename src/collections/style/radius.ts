703e6953ab75318844570c1ab377eb804952f8ef
Onoko <hello@onoko.dev>
2026-01-10 19:23:58 +0100

    typography && radius

--- src/collections/style/radius.ts
+++ src/collections/style/radius.ts
@@ -1,7 +1,25 @@
+import { SCOPES } from "../../constants/figmaConstants";
 import { FigmaCollection, FigmaVariable } from "../../types";
+import { generateVariable } from "../../utils/figmaUtils";
 import { generateModeJson } from "../../utils/jsonUtils";
 
-const variables: Record<string, Record<string, FigmaVariable>> = {};
+const variables: { [key: string]: FigmaVariable } = {};
+
+const config = {
+  sm: 4,
+  md: 8,
+  lg: 16,
+  xl: 24,
+  "2xl": 32,
+  rounded: 9999,
+};
+
+for (const [key, value] of Object.entries(config)) {
+  variables[key] = generateVariable("number", value, [
+    SCOPES.FLOAT.CORNER_RADIUS,
+  ]);
+}
+
 const mode = "Value";
 const collectionName = "Style/Radius";
 export const radiusCollection: FigmaCollection = {
