de701b804c0a82f4b3762fb9aca4ea2facbf362c
Onoko <hello@onoko.dev>
2026-01-27 10:45:47 +0100

    refactor: enhance error handling and logging in generateRadius function

--- src/main/builders/variables/styles/RadiusBuilder.ts
+++ src/main/builders/variables/styles/RadiusBuilder.ts
@@ -1,23 +1,43 @@
 import { SCOPES } from "../../../constants/variablesConstants";
 import { VariableConfig } from "../../../types/variablesTypes";
+import { logger } from "../../../utils/logger";
 import { variableBuilder } from "../variableBuilder";
 
 export async function generateRadius(
   radius: Record<string, number>,
 ): Promise<Variable[]> {
-  const variables: VariableConfig[] = [];
-  radius["square"] = 0;
-  radius["rounded"] = 9999;
+  try {
+    const variables: VariableConfig[] = [];
+    radius["square"] = 0;
+    radius["rounded"] = 9999;
 
-  for (const [name, value] of Object.entries(radius)) {
-    variables.push({
-      name: name.toLowerCase(),
-      collection: "Style\\Radius",
-      type: "FLOAT",
-      value,
-      scopes: [SCOPES.FLOAT.CORNER_RADIUS],
-    });
+    for (const [name, value] of Object.entries(radius)) {
+      variables.push({
+        name: name.toLowerCase(),
+        collection: "Style\\Radius",
+        type: "FLOAT",
+        value,
+        scopes: [SCOPES.FLOAT.CORNER_RADIUS],
+      });
+    }
+
+    const newVariables =
+      await variableBuilder.createOrUpdateVariables(variables);
+    if (newVariables.length === 0) {
+      logger.error(
+        "[generateRadius] Aucune variable de radius créée ou mise à jour.",
+      );
+      throw new Error("Aucune variable de radius créée ou mise à jour.");
+    }
+    logger.success(
+      `[generateRadius] ${newVariables.length} variables de radius créées ou mises à jour avec succès.`,
+    );
+    return newVariables;
+  } catch (error) {
+    logger.error(
+      "[generateRadius] Erreur lors de la génération des variables de radius:",
+      error,
+    );
+    throw error;
   }
-
-  return await variableBuilder.createOrUpdateVariables(variables);
 }
